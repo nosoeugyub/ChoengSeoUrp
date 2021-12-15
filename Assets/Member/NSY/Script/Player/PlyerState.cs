e94bc824ac34f5a7b337c45306b749dd4902d13b
Dimu <dimu0302@gmail.com>
2021-12-15 15:34:17 +0900

    fab fix, script fix, scene setting

--- Assets/Member/NSY/Script/Player/PlyerState.cs
+++ Assets/Member/NSY/Script/Player/PlyerState.cs
@@ -12,7 +12,7 @@ namespace Player.State
         [SerializeField]
         protected string PlayerClassName = "PlayerState";
 
-        [SerializeField] private Transform cameraArm;
+        //[SerializeField] private Transform cameraArm;
 
 
 
@@ -61,27 +61,27 @@ namespace Player.State
         }
         protected virtual void Update()
         {
-            LookAround();
+            //LookAround();
         }
 
 
         //소정씨 코드
-        private void LookAround()
-        {
-            Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-            Vector3 camAngle = cameraArm.rotation.eulerAngles;
-            float x = camAngle.x - mouseDelta.y;
+        //private void LookAround()
+        //{
+        //    Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        //    Vector3 camAngle = cameraArm.rotation.eulerAngles;
+        //    float x = camAngle.x - mouseDelta.y;
 
-            if (x < 180f)
-            {
-                x = Mathf.Clamp(x, -1f, 70f);
-            }
-            else
-            {
-                x = Mathf.Clamp(x, 335f, 361f);
-            }
-            cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
-        }
+        //    if (x < 180f)
+        //    {
+        //        x = Mathf.Clamp(x, -1f, 70f);
+        //    }
+        //    else
+        //    {
+        //        x = Mathf.Clamp(x, 335f, 361f);
+        //    }
+        //    cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
+        //}
 
 
 
