3ae11e76d7b25c3eaec624290ede1ec22e10abc6
Zess <maixmagus@gmail.com>
2021-12-18 04:04:09 +0900

    Camera Update

---
+++ Assets/Member/LVTT/CScript/Camera/CameraManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+namespace Game.Cam
+{ public class CameraManager : MonoBehaviour
+    {
+        [SerializeField]
+        private CinemachineVirtualCamera [] virtualCamera = null;
+        // Start is called before the first frame update
+        void Start()
+        {
+            DeactiveAllSubCam();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.Q))
+            {
+                ActiveCamera(0);
+            }
+
+            if(Input.GetKey(KeyCode.E))
+            {
+                DeactiveCamera(0);
+            }
+        }
+
+        void DeactiveAllSubCam()
+        {
+            for(int i=0;i<virtualCamera.Length;i++)
+            {
+                virtualCamera[i].enabled = false;
+            }
+           
+        }
+        
+        void ActiveCamera(int camNum)
+        {
+            virtualCamera[camNum].enabled = true;
+        }
+
+        void DeactiveCamera(int camNum)
+        {
+            virtualCamera[camNum].enabled = false;
+        }
+    }
+}
+
