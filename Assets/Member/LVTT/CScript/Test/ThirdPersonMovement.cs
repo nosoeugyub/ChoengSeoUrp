1ffdfa33bb62cf1008438391be1edf89d4041fc7
Zess <maixmagus@gmail.com>
2022-01-15 17:59:16 +0900

    페이페씬 Update

---
+++ Assets/Member/LVTT/CScript/Test/ThirdPersonMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace LVTT.Test
+{
+    public class ThirdPersonMovement : MonoBehaviour
+    {
+    CharacterController controller;
+
+        public float speed=6f;
+
+        private void Start()
+        {
+            controller = GetComponent<CharacterController>();
+        }
+        void Update()
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+            if(direction.normalized.magnitude>=0.1f)
+            {
+                controller.Move(direction * speed * Time.deltaTime);
+            }
+        }
+    }
+
+}
