3d9b92d5bf35899c8ec7727e89ad499a78004a29
Noh Seoug Yub <dkzkdlsn12@gmail.com>
2022-02-15 03:37:06 +0900

    경량화작업

---
+++ Assets/Member/NSY/Script/Game/Forest/Tree.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Tree : TreeModel
+{
+    TreeModel treemodel; //나무 모델을 상속
+
+    Vector3 TreePosition; //나무의 위치
+    protected Vector3 FruitPosition;//열매 생성위치
+
+
+
+}
---
+++ Assets/Member/NSY/Script/Game/Forest/TreeModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class TreeModel : MonoBehaviour//공통된 나무 속성을 관리하는 클래스
+{
+    [Header("공유된 너무들의 속성")]
+    [SerializeField]
+    protected Texture TreeTexture;
+    [SerializeField]
+    protected Texture ShadowTreeTexture;
+    [SerializeField]
+    protected Material TreeMesh;
+    [SerializeField]
+    protected Material ShadowMesh;
+
+
+   
+  
+}
