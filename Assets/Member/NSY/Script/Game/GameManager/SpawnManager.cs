3c4d99d7dc55abe2450aca6634d66319fda85d79
Noh Seoug Yub <dkzkdlsn12@gmail.com>
2022-02-19 02:59:29 +0900

    코드정리

---
+++ Assets/Member/NSY/Script/Game/GameManager/SpawnManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public Transform[] SpawnPos;
+
+
+  IEnumerator SpawnTrash()
+    {
+        yield return new WaitForSeconds(10f);
+        GameObject PaperTrash = ObjectPooling.SpawnFromPool("PaperTrash", SpawnPos[0].transform.position);
+       
+    }
+}
