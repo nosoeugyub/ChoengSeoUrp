a43a189f6935f7757a3e3fcd1fcbe252c27716cc
Zess <maixmagus@gmail.com>
2022-02-25 01:29:08 +0900

    MapTravel 수정

--- Assets/Member/LVTT/CScript/Map/MapUI.cs
+++ Assets/Member/LVTT/CScript/Map/MapUI.cs
@@ -5,13 +5,13 @@ namespace TT.MapTravel
 {
     public class MapUI : MonoBehaviour
     {
-        [SerializeField] GameObject MapTravelUI;
-        bool MapUIisOn;
+        //[SerializeField] GameObject MapTravelUI;
+        //bool MapUIisOn;
         PlayerMoveMent PlayerMovement;
         CameraManager CamManager;
          void Start()
         {
-            MapUIisOn = false;
+            //MapUIisOn = false;
             PlayerMovement = FindObjectOfType<PlayerMoveMent>();
             CamManager = FindObjectOfType<CameraManager>();
         }
@@ -26,143 +26,143 @@ namespace TT.MapTravel
            
         }
 
-       public void OpenandCloseMapUI()
-        {
-            switch (MapUIisOn)
-            {
-                case true:
-                    MapTravelUI.SetActive(false);
-                    MapUIisOn = false;
-                    break;
-                case false:
-                    MapTravelUI.SetActive(true);
-                    MapUIisOn = true;
-                    break;
-            }
+       //public void OpenandCloseMapUI()
+       // {
+       //     switch (MapUIisOn)
+       //     {
+       //         case true:
+       //             MapTravelUI.SetActive(false);
+       //             MapUIisOn = false;
+       //             break;
+       //         case false:
+       //             MapTravelUI.SetActive(true);
+       //             MapUIisOn = true;
+       //             break;
+       //     }
 
-        }
+       // }
 
         ////Outer////
         public void BtnTravelToOuterArea1()
         {
-            OpenandCloseMapUI();
+            //OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 0;
             PlayerMovement.Maptravel = true;
         }
 
         public void BtnTravelToOuterArea2()
         {
-            OpenandCloseMapUI();
+            //OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 1;
             PlayerMovement.Maptravel = true;
         }
 
         public void BtnTravelToOuterArea3()
         {
-            OpenandCloseMapUI();
+            //OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 2;
             PlayerMovement.Maptravel = true;
         }
 
         public void BtnTravelToOuterArea4()
         {
-            OpenandCloseMapUI();
+            //OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 3;
             PlayerMovement.Maptravel = true;
         }
 
         public void BtnTravelToOuterArea5()
         {
-            OpenandCloseMapUI();
+            //OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 4;
             PlayerMovement.Maptravel = true;
         }
 
         public void BtnTravelToOuterArea6()
         {
-            OpenandCloseMapUI();
+            //OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 5;
             PlayerMovement.Maptravel = true;
         }
 
         public void BtnTravelToOuterArea7()
         {
-            OpenandCloseMapUI();
+            //OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 6;
             PlayerMovement.Maptravel = true;
         }
 
         public void BtnTravelToOuterArea8()
         {
-            OpenandCloseMapUI();
+            //OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 7;
             PlayerMovement.Maptravel = true;
         }
         ////Inner////
         public void BtnTravelToInnerArea1()
         {
-            OpenandCloseMapUI();
+            //OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 8;
             PlayerMovement.Maptravel = true;
         }
 
         public void BtnTravelToInnerArea2()
         {
-            OpenandCloseMapUI();
+            //OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 9;
             PlayerMovement.Maptravel = true;
         }
         public void BtnTravelToInnerArea3()
         {
-            OpenandCloseMapUI();
+            //OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 10;
             PlayerMovement.Maptravel = true;
         }
         public void BtnTravelToInnerArea4()
         {
-            OpenandCloseMapUI();
+            //OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 11;
             PlayerMovement.Maptravel = true;
         }
         public void BtnTravelToInnerArea5()
         {
-            OpenandCloseMapUI();
+           // OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 12;
             PlayerMovement.Maptravel = true;
         }
         public void BtnTravelToInnerArea6()
         {
-            OpenandCloseMapUI();
+            //OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 13;
             PlayerMovement.Maptravel = true;
         }
         public void BtnTravelToInnerArea7()
         {
-            OpenandCloseMapUI();
+            //OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 14;
             PlayerMovement.Maptravel = true;
         }
         public void BtnTravelToInnerArea8()
         {
-            OpenandCloseMapUI();
+            //OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 15;
             PlayerMovement.Maptravel = true;
         }
         public void BtnTravelToInnerArea9()
         {
-            OpenandCloseMapUI();
+            //OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 16;
             PlayerMovement.Maptravel = true;
         }
         public void BtnTravelToInnerArea10()
         {
-            OpenandCloseMapUI();
+            //OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 17;
             PlayerMovement.Maptravel = true;
         }
         public void BtnTravelToInnerArea11()
         {
-            OpenandCloseMapUI();
+            //OpenandCloseMapUI();
             PlayerMovement.curAreaNum = 18;
             PlayerMovement.Maptravel = true;
         }
