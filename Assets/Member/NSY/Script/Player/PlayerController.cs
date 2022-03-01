52767f9680111d1828fd865ac03f7be61a5c4cdd
Noh Seoug Yub <dkzkdlsn12@gmail.com>
2022-03-01 19:10:17 +0900

    코드 데이터 분배

--- Assets/Member/NSY/Script/Player/PlayerController.cs
+++ Assets/Member/NSY/Script/Player/PlayerController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using NSY.Iven;
 using DM.Inven;
+using System;
+using UnityEngine.UI;
+
+
 //부모 클래스
 namespace NSY.Player
 {
@@ -37,17 +41,25 @@ namespace NSY.Player
         // 이벤트 드래그 엔 드롭
         [SerializeField] InventoryNSY inventory;
         [SerializeField] EquipPanel equipmentPanel;
-        //스텟 판넬 추가
+        [SerializeField] Image DraggableItem;
 
         private void Awake()
         {
             //장착 관련 이벤
             inventory.OnRightClickEvent += Equip;
-            equipmentPanel.OnRightClickEvent += UneQuip;
+            equipmentPanel.OnRightClickEvent += UnEquip;
             //드래그 이벤트
             inventory.OnBeginDragEvent += BeginDrag;
             equipmentPanel.OnBeginDragEvent += BeginDrag;
-            //1227
+            //드래그 끝 이벤트
+            inventory.OnEndDragEvent += EndDrag;
+            equipmentPanel.OnEndDragEvent += EndDrag;
+            //드래근
+            inventory.OnDragEvent += Drag;
+            equipmentPanel.OnDragEvent += Drag;
+            //드롭
+            inventory.OnDropEvent += Drop;
+            equipmentPanel.OnDropEvent += Drop;
         }
         // Start is called before the first frame update
         void Start()
@@ -66,11 +78,69 @@ namespace NSY.Player
         }
         
      
+        //드래그 엔 드롭 이벤트 함수
+        public void Equip(ItemSlot itemslot )
+        {
+            Item EquippableItem = itemslot.item as Item;
+            if (EquippableItem != null)
+            {
+                Equip(EquippableItem);
+            }
+        }
 
+        private void Equip(Item equippableItem)
+        {
+            if (inventory.RemoveItem(equippableItem))
+            {
+                Item previousItem;
+                if (equipmentPanel.AddItem(equippableItem, out previousItem))
+                {
+                    if (previousItem != null)
+                    {
+                        inventory.AddItem(previousItem);
+                        // previousItem.UnEquip(this);
 
+                    }
+                    //  Item.Equals(this);
+                }
+            }
+        }
 
+        public void UnEquip(ItemSlot itemslot)
+        {
+            Item EquippableItem = itemslot.item as Item;
+            if (EquippableItem != null)
+            {
+                UnEquip(EquippableItem);
+            }
+        }
 
+        private void UnEquip(Item equippableItem)
+        {
+            throw new NotImplementedException();
+        }
 
+     
+
+        private void BeginDrag(ItemSlot itemSlot)
+        {
+            if (itemSlot.item != null)
+            {
+
+            }
+        }
+        private void EndDrag(ItemSlot itemSlot)
+        {
+
+        }
+        private void Drag(ItemSlot itemSlot)
+        {
+
+        }
+        private void Drop(ItemSlot itemSlot)
+        {
+
+        }
     }
 }
 
