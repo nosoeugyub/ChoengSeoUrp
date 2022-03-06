e1d1ec1173814c9abab7820961cd30d4f04f879e
Noh Seoug Yub <dkzkdlsn12@gmail.com>
2022-03-06 21:57:16 +0900

    인벤토리수정

---
+++ Assets/Member/NSY/Script/Game/Recipe/CraftingWindow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using DM.Inven;
+namespace NSY.Iven
+{
+    public class CraftingWindow : MonoBehaviour
+    {
+        [Header("Referneces")]
+        [SerializeField] CraftingRecipeUI recipeUIPrefab;
+        [SerializeField] RectTransform recipeUIParent;
+        [SerializeField] List<CraftingRecipeUI> craftingRecipeUis;
+
+        [Header("Pubilic Variables")]
+        public ItemContainer ItemContainer;
+        public List<CraftingRecipe> CraftingRecipes;
+
+        public event Action<BaseItemSlot> OnPointerEnterEvent;
+        public event Action<BaseItemSlot> OnPointerExitEvent;
+
+
+        public void OnValidate()
+        {
+            Init();
+        }
+
+        private void Start()
+        {
+            Init();
+
+            foreach (CraftingRecipe craftingRecipeUI in craftingRecipeUis)
+            {
+
+            }
+        }
+    }
+
+}
+
