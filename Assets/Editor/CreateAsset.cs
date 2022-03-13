80facc2db07707f0ed30889d820a775f9d025efe
Kim Chaewon <48170768+kimdimu@users.noreply.github.com>
2022-03-14 05:25:33 +0900

    에디터 연구중

---
+++ Assets/Editor/CreateAsset.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CreateAsset : MonoBehaviour
+{
+    [MenuItem("Assets/CreateAssets")]
+    static void CreateAssets()
+    {
+        Object[] _textures = Selection.GetFiltered(typeof(PrefabUtility), SelectionMode.DeepAssets);
+
+
+        Material material = new Material(Shader.Find("Specular"));
+        AssetDatabase.CreateAsset(material, "Assets/MyMaterial.mat");
+
+        Debug.Log(AssetDatabase.GetAssetPath(material));
+    }
+
+}
