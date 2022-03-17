8c8c57a24682dd6080aa1b4ce96e38d644e4089a
Kim Chaewon <48170768+kimdimu@users.noreply.github.com>
2022-03-18 02:59:24 +0900

    리소스 자동생성 기능 구현

--- Assets/Editor/CreateAsset.cs
+++ Assets/Editor/CreateAsset.cs
@@ -6,21 +6,33 @@ public class CreateAsset : MonoBehaviour
     [MenuItem("Assets/CreateAssets")]
     static void CreateAssets()
     {
+        int count = 0;
         Object[] _textures = Selection.GetFiltered(typeof(Texture2D), SelectionMode.DeepAssets);
-        Material material = new Material(Resources.Load<Material>("Material/BaseMat"));
+        Material material;// = new Material(Resources.Load<Material>("BaseMat"));
+        //prefabUtility = Resources.Load<GameObject>("ItemBase");
 
         foreach (Texture2D texture in _textures)
         {
+            count++;
             string path = AssetDatabase.GetAssetPath(texture);
 
-            //Material material = new Material(Shader.Find("Universal Rander Pipeline"));
+            material = new Material(Resources.Load<Material>("BaseMat"));
+            Debug.Log(AssetDatabase.GetAssetPath(material));
 
-            //Material material = new Material(Shader.Find("Unlit"));
-            //string name = "DimuMat";
-            AssetDatabase.CreateAsset(material, string.Format("{0}.mat", path));
+            var modelRootGO = Resources.Load<GameObject>("ItemBase");//(GameObject)AssetDatabase.LoadMainAssetAtPath("Assets/Resources/ItemBase.prefab");
+            var instanceRoot = PrefabUtility.InstantiatePrefab(modelRootGO);
+            GameObject variantRoot = PrefabUtility.SaveAsPrefabAsset((GameObject)instanceRoot, string.Format("{0}{1}.prefab", path,count));
+            Transform variantRootChild = variantRoot.transform.GetChild(0);
 
-            material.SetTexture("_MainTex", texture);
-            
+            //PrefabUtility.SaveAsPrefabAssetAndConnect(Resources.Load<GameObject>("ItemBase"), path, InteractionMode.UserAction);
+
+            AssetDatabase.CreateAsset(material, string.Format("{0}{1}.mat", path, count));
+
+            material.SetTexture("_BaseMap", texture);
+
+            variantRootChild.GetComponent<MeshRenderer>().material = material;
+            variantRootChild.localScale = new Vector2(texture.width*0.01f, texture.height*0.01f);
+
             Debug.Log(AssetDatabase.GetAssetPath(material));
             //return;
         }
