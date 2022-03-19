abf415fce5bd5a472fb31515743ddf0b03fd3ece
Kim Chaewon <48170768+kimdimu@users.noreply.github.com>
2022-03-20 05:01:18 +0900

    새로운 리소스 추가

--- Assets/Editor/CreateAsset.cs
+++ Assets/Editor/CreateAsset.cs
@@ -6,28 +6,26 @@ public class CreateAsset : MonoBehaviour
     [MenuItem("Assets/CreateAssets")]
     static void CreateAssets()
     {
-        int count = 0;
+        //int count = 0;
         Object[] _textures = Selection.GetFiltered(typeof(Texture2D), SelectionMode.DeepAssets);
         Material material;// = new Material(Resources.Load<Material>("BaseMat"));
         //prefabUtility = Resources.Load<GameObject>("ItemBase");
 
         foreach (Texture2D texture in _textures)
         {
-            count++;
+            //count++;
             string path = AssetDatabase.GetAssetPath(texture);
 
             material = new Material(Resources.Load<Material>("BaseMat"));
-            Debug.Log(AssetDatabase.GetAssetPath(material));
 
             var modelRootGO = Resources.Load<GameObject>("ItemBase");//(GameObject)AssetDatabase.LoadMainAssetAtPath("Assets/Resources/ItemBase.prefab");
             var instanceRoot = PrefabUtility.InstantiatePrefab(modelRootGO);
-            GameObject variantRoot = PrefabUtility.SaveAsPrefabAsset((GameObject)instanceRoot, string.Format("{0}{1}.prefab", path,count));
+            GameObject variantRoot = PrefabUtility.SaveAsPrefabAsset((GameObject)instanceRoot, string.Format("{0}.prefab", path));
             Transform variantRootChild = variantRoot.transform.GetChild(0);
 
             //PrefabUtility.SaveAsPrefabAssetAndConnect(Resources.Load<GameObject>("ItemBase"), path, InteractionMode.UserAction);
 
-            AssetDatabase.CreateAsset(material, string.Format("{0}{1}.mat", path, count));
-
+            AssetDatabase.CreateAsset(material, string.Format("{0}.mat", path));
             material.SetTexture("_BaseMap", texture);
 
             variantRootChild.GetComponent<MeshRenderer>().material = material;
