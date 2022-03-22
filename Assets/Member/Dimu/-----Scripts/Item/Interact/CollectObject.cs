e989908eeb9da6d12a1392c41419b1db6597eec7
Noh Seoug Yub <dkzkdlsn12@gmail.com>
2022-03-22 22:57:09 +0900

    멕시멈카운트 오류 해결

--- Assets/Member/Dimu/-----Scripts/Item/Interact/CollectObject.cs
+++ Assets/Member/Dimu/-----Scripts/Item/Interact/CollectObject.cs
@@ -2,6 +2,7 @@
 
 public class CollectObject : ItemObject, ICollectable
 {
+    public int amount = 1;
     public string CanInteract()
     {
         return "줍기";
@@ -9,7 +10,17 @@ public class CollectObject : ItemObject, ICollectable
 
     public void Collect()
     {
-        inventoryNSY.AddItem(item);
+        Item itemCopy = item.GetCopy();
+        if (inventoryNSY.AddItem(itemCopy))
+        {
+            amount--;
+           
+        }
+        else
+        {
+            itemCopy.Destroy();
+
+        }
 
         Interact();
     }
