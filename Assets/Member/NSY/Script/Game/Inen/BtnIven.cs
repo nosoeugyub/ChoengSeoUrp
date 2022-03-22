e16a802e5e7e6152388ab7ccaf47f29c2e541ca0
Noh Seoug Yub <dkzkdlsn12@gmail.com>
2022-03-22 22:18:26 +0900

    ui제작

--- Assets/Member/NSY/Script/Game/Inen/BtnIven.cs
+++ Assets/Member/NSY/Script/Game/Inen/BtnIven.cs
@@ -8,30 +8,74 @@ public class BtnIven : MonoBehaviour
     [SerializeField] GameObject EquionmentPanel;
     [SerializeField] GameObject iteminfoPanel;
     [SerializeField] GameObject CraftPanel;
+    [SerializeField] GameObject RecipeListPanel;
 
-   public bool isInven = true;
+    [SerializeField] GameObject iventroy;
+
+
+
+    //레시피목록
+    [Header("레시피목록")]
+    [SerializeField] GameObject ToolBtn;
+
+    public bool isInven = true;
    public bool Craft;
-
+    public bool Recipe;
+    // 텝 버튼
     public void IvenBtnClick()
     {
         isInven = true;
         Craft = false;
+        Recipe = false;
         if (isInven)
         {
+            iventroy.SetActive(true);
+
             CraftPanel.SetActive(false);
             EquionmentPanel.SetActive(true);
             iteminfoPanel.SetActive(true);
+            RecipeListPanel.SetActive(false);
         }
     }
     public void CraftBtnClick()
     {
         isInven = false;
         Craft = true;
+        Recipe = false;
         if (Craft)
         {
+            iventroy.SetActive(true);
+
             CraftPanel.SetActive(true);
             EquionmentPanel.SetActive(false);
             iteminfoPanel.SetActive(false);
+            RecipeListPanel.SetActive(false);
         }
+    } 
+
+    public void RecipeListClick()
+    {
+        isInven = false;
+        Craft = false;
+        Recipe = true;
+        if(Recipe)
+        {
+            iventroy.SetActive(false);
+
+            CraftPanel.SetActive(false);
+            EquionmentPanel.SetActive(false);
+            iteminfoPanel.SetActive(false);
+            RecipeListPanel.SetActive(true);
+        }
+
     }
+
+
+
+    //레시피 목록
+    public void ToolBtnClick()
+    {
+        ToolBtn.SetActive(true);
+    }
+
 }
