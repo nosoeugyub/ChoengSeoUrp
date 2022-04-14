b869841a4239634afeaaff921c010b8f0ec68ff4
Noh Seoug Yub <dkzkdlsn12@gmail.com>
2022-04-14 19:31:26 +0900

    메타파일

---
+++ Assets/Member/NSY/Script/Bug/RestAndStart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace NSY.Bug
+{
+    public class RestAndStart : Bugstats//날아다니는 벌래들이 앉는 상태   
+    {
+        public override void Enter( )
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Execute( )
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Exit( )
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
