87555f1b6fce746142aac8885696fa31525d0d7a
Noh Seoug Yub <dkzkdlsn12@gmail.com>
2022-04-14 19:32:08 +0900

    버그고침

--- Assets/Member/NSY/Script/Bug/RestAndStart.cs
+++ Assets/Member/NSY/Script/Bug/RestAndStart.cs
@@ -7,8 +7,20 @@ namespace NSY.Bug
 {
     public class RestAndStart : Bugstats
     {
+        public override void Enter(Bug_fly bugfly)
+        {
+            throw new System.NotImplementedException();
+        }
 
+        public override void Execute(Bug_fly bugfly)
+        {
+            throw new System.NotImplementedException();
+        }
 
+        public override void Exit(Bug_fly bugfly)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 
 }
