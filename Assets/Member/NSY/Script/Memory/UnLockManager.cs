22f3ca99bbf69c7d91bfd3181bfcc789549d3206
nosoeugyub <74639167+nosoeugyub@users.noreply.github.com>
2022-05-06 06:45:50 +0900

    조건테이블

--- Assets/Member/NSY/Script/Memory/UnLockManager.cs
+++ Assets/Member/NSY/Script/Memory/UnLockManager.cs
@@ -1,9 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
 
 public class UnLockManager : MonoBehaviour
 {
-    
-    
+    [SerializeField] UnLockTable unlocktable;
+
+
+    //조건함수 이벤트
+    public delegate bool getitemUnlock(int itemAmount);
+    public static event getitemUnlock GetItemUnlock; //아이템 흭득시 아이템 넘버를 받아와서 조건탐색
+
+    public delegate bool getRecipeUnlock(Item item);
+    public static event getRecipeUnlock GetRecipeUnlock; //레시피 흭득시 레시피 넘버를 받아와서 조건탐색
+
+
+
+
+
+   
 }
--- Assets/Member/NSY/Script/Memory/UnLockTable.cs
+++ Assets/Member/NSY/Script/Memory/UnLockTable.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+
 //조건상세테이블
-[CreateAssetMenu(fileName = "Lockitems", menuName = "LockItem")]
+[CreateAssetMenu(fileName = "Lockitems", menuName = "LockItemDatatable")]
 
 public class UnLockTable : ScriptableObject
 {
+   
     //언락에 성공한 아이템 식별자
-     
-
+    [Header("item 스크립트의 numbering을 적으면 여기에나옴 \n" +
+        "해금 레시피 아이템 식별 넘버임")]
+    [Space(10)]
+    public int itemIdNubering;
+    [Space(10)]
+    [Header("해제에 필요한 아이템들")]
+    public List<UnlcokIteminfo>  FindLockItem;
 
 
     //언락에 필요한 아이템
+    [Header("해제 될 레시피")]
+    [SerializeField]
     private Item _LockItem;
+
     public Item LockItem
     {
         get
@@ -23,9 +35,36 @@ public class UnLockTable : ScriptableObject
         set
         {
             _LockItem = value;
+            itemIdNubering = _LockItem.ItemNubering; //아잉템 넘버
+            if (_LockItem != null)
+            {
+                FindLockItem.AddRange(_LockItem.UnlcokIteminfos);
+
+
+
+            }
+           
+        }
+    }
+
+        //언락 필요한 퀘스트 넘버 프로퍼티
+        private int _QuestNubering;
+        public int QuestNubering
+        {
+            get
+            {
+                return _QuestNubering;
+            }
+            set
+            {
+                _QuestNubering = value;
+            }
         }
 
-
-        //언락 필요한 퀘스트 프로퍼티
+    private void OnValidate()
+    {
+        QuestNubering = _QuestNubering;
+        LockItem = _LockItem;
     }
+
 }
