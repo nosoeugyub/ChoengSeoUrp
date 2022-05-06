81a748c765fcfce68e1c271e1540d5bc846db5a4
nosoeugyub <74639167+nosoeugyub@users.noreply.github.com>
2022-05-06 19:31:06 +0900

    저장

--- Assets/Member/NSY/Script/Memory/UnLockManager.cs
+++ Assets/Member/NSY/Script/Memory/UnLockManager.cs
@@ -8,25 +8,35 @@ public class UnLockManager : MonoBehaviour
 {
     [Header("조건을 채울 컴포넌트들")]
 
-    [SerializeField] UnLockTable unlocktable;
+  //  [SerializeField] UnLockTable unlocktable;
     [SerializeField] InventoryNSY inventory;
 
     //조건함수 이벤트
-    public delegate bool getitemUnlock(Item item);
+    public delegate bool getitemUnlock(UnLockTable item);
     public static event getitemUnlock GetItemUnlock; //아이템 흭득시 아이템 넘버를 받아와서 조건탐색
 
-    public delegate bool getRecipeUnlock(Item item);
+    public delegate bool getRecipeUnlock(UnLockTable item);
     public static event getRecipeUnlock GetRecipeUnlock; //레시피 흭득시 레시피 넘버를 받아와서 조건탐색
 
 
-    //조건 갯수
-    public bool GetAmountLock()
+    //조건 갯수 ex) 일정 아이템을 몇개이상 얻었을때 해금
+    public bool GetAmountLock(UnLockTable unlocktalbeitem )
     {
-
-        foreach (ItemSlot item in inventory.ItemSlots)
+        for (int i = 0; i < inventory.ItemSlots.Count; i++)
         {
+            for (int j = 0; j < unlocktalbeitem.FindLockItem.Count; j++)
+            {
+                if (true)
+                {
 
+                    return true;
+                }
+
+
+              
+            }
         }
+     
 
         return false;
     }
