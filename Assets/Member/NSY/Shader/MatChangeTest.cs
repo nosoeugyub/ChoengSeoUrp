f503449bdf5c6f3a5b767dfc274331433d359a9c
nosoeugyub <74639167+nosoeugyub@users.noreply.github.com>
2022-05-19 19:33:24 +0900

    Glow 제작 완료

---
+++ Assets/Member/NSY/Shader/MatChangeTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatChangeTest : MonoBehaviour
+{
+    public Material OrginMat;
+    public Material ChangeMat;
+
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.O))
+        {
+
+        }
+    }
+}
