2f3c37dac16199908b2744f3571fe7fc2534cd65
Zess <maixmagus@gmail.com>
2022-05-20 21:51:19 +0900

    Credit 씬

---
+++ Assets/Member/LVTT/CScript/Credit/CameraCredit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Cam
+{
+    public class CameraCredit : MonoBehaviour
+    {
+        [SerializeField] GameObject mainCam;
+        [SerializeField] CameraManager CamManager;
+        [SerializeField] Animator WhiteScreenAnim;
+        void Start()
+        {
+            Invoke("Zoomout", 0.5f);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void Zoomout()
+        {
+            //CamManager.DeactiveSubCamera();
+            CamManager.ActiveSubCamera(1);
+            WhiteScreenAnim.SetTrigger("startwhitescreen");
+        }
+    }
+
+}
