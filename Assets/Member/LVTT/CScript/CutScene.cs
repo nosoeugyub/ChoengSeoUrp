11b8d2912c7beac7732bd4d00093cbf574df9430
Zess <maixmagus@gmail.com>
2022-06-05 03:01:19 +0900

    CutScene

---
+++ Assets/Member/LVTT/CScript/CutScene.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class CutSceneImage
+{
+    public bool isOpen=false;
+    public Image img;
+    public Image LockedImage;
+}
+public class CutScene : MonoBehaviour
+{
+    public CutSceneImage [] Image;
+   
+    [SerializeField] Transform ImageSpawn;
+    [SerializeField] Button ConfirmButton;
+    [SerializeField] GameObject CutSceneLibrary;
+    Image curImage;
+
+    
+    public void PrintImage(int index)//Calling this will also unlock the Image in the Library
+    {
+        if(Image[index]!=null&&!Image[index].isOpen)
+        {
+            Image[index].isOpen = true;
+            Image[index].LockedImage.gameObject.SetActive(false);
+            curImage = Instantiate(Image[index].img, ImageSpawn.position,ImageSpawn.rotation,ImageSpawn);
+            curImage.rectTransform.sizeDelta = new Vector2(1920, 1080);
+            Invoke("ShowConfirmButton", 3f);
+          
+        }
+     
+    }  
+    
+    public void OpenImage(int index) //Open the unlocked Image and show the "확인" button without 3sec waiting ->Use in Image Library
+    {
+        if (Image[index] != null && !Image[index].isOpen)
+        {
+            //SetLibraryStatus(false);
+            Image[index].isOpen = true;
+            curImage = Instantiate(Image[index].img, ImageSpawn.position, ImageSpawn.rotation, ImageSpawn);
+            curImage.rectTransform.sizeDelta = new Vector2(1920, 1080);
+            ShowConfirmButton();
+
+        }
+    }    
+    public void CloseImage()
+    {
+        Destroy(curImage.gameObject);
+        ConfirmButton.gameObject.SetActive(false);
+         foreach(CutSceneImage image in Image)
+        {
+            image.isOpen = false;
+        }
+
+    }
+    void ShowConfirmButton()
+    {
+        ConfirmButton.gameObject.SetActive(true);
+    }
+
+    public void SetLibraryStatus (bool status)
+    {
+        CutSceneLibrary.SetActive(status);
+    }   
+    
+    public void CloseCutSceneLibrary()
+    {
+
+    }    
+}
