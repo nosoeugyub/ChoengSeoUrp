bc6602f2f89a5559c448d9b3947f8f79498779cf
nosoeugyub <74639167+nosoeugyub@users.noreply.github.com>
2022-06-08 23:44:16 +0900

    스키린샷자유저장 (1.2)

--- Assets/Member/NSY/Script/TakeScreenShot/Screenshot.cs
+++ Assets/Member/NSY/Script/TakeScreenShot/Screenshot.cs
@@ -12,10 +12,37 @@ public class Screenshot : MonoBehaviour
 {
     public List<PageView> PageViows; //엘범 페이지
 
-    [Header("머테리얼")]
+    [Header("모두의 엘범")]
     [SerializeField]
-    private List<Material> PageMat;
+    private List<Material> EveryPageMat;
 
+    [Header("1페이지 머테리얼")]
+    [SerializeField]
+    private List<Material> PageMat1;
+
+    [Header("2페이지 머테리얼")]
+    [SerializeField]
+    private List<Material> PageMat2;
+
+     [Header("3페이지 머테리얼")]
+    [SerializeField]
+    private List<Material> PageMat3;
+
+    [Header("4페이지 머테리얼")]
+    [SerializeField]
+    private List<Material> PageMat4;
+
+    [Header("5페이지 머테리얼")]
+    [SerializeField]
+    private List<Material> PageMat5;
+
+    [Header("6페이지 머테리얼")]
+    [SerializeField]
+    private List<Material> PageMat6;
+
+    [Header("7페이지 머테리얼")]
+    [SerializeField]
+    private List<Material> PageMat7;
 
     public Camera camera;//ui카메라가찍히기 전에 게임 카메라에 있는 랜더텍스쳐를 활용해 ui제외한 부분들을 스크린샷처럼 찍음 
     public int Width; //가로
@@ -37,9 +64,6 @@ public class Screenshot : MonoBehaviour
             return Application.dataPath;
         }
     }
-
-   
-   
 
     [SerializeField]
     private Texture2D texture1;
@@ -128,36 +152,55 @@ public class Screenshot : MonoBehaviour
             byte[] byteArray = screenshotTexture1.EncodeToPNG(); // 이미지 저장
             byte[] byteArray2 = screenshotTexture2.EncodeToPNG(); // 이미지 저장
 
-            for (int i = 0; i < SuperManager.Instance.buildingManager.buildings.Count; i++)
+            //Ver 2  . 무지성 저장
+            #region
+            for (int i = 0; i < EveryPageMat.Count; i++)
             {
-                Debug.Log(SuperManager.Instance.buildingManager.buildings[i]._livingCharacter);
-                if (PageMat[i].GetTexture("_MainTex") == null)
+                if (EveryPageMat[i].GetTexture("_MainTex") != null)
                 {
-                    if (SuperManager.Instance.buildingManager.buildings[i]._livingCharacter != null)
-                    {
-                        Debug.Log("김태리 김채원 닮음");
-                        PageMat[i].SetTexture("_MainTex", Texutre2);
-                        PageMat[i + 1].SetTexture("_MainTex", Texutre1);
-                        camera.targetTexture = null;
-                        Destroy(RenderTexture);
-                        return;
-                    }
-                    else
-                    {
-                        Debug.Log("개씨발");
-
-                        camera.targetTexture = null;
-                        Destroy(RenderTexture);
-                        continue;
-                    }
-                }
-                else
+                    Debug.Log("테리우스 김채원 닮음");
                     continue;
-               
-               
+                }
+                if (EveryPageMat[i].GetTexture("_MainTex") == null)
+                {
+                    EveryPageMat[i].SetTexture("_MainTex", Texutre2);
+                    EveryPageMat[i+1].SetTexture("_MainTex", Texutre1);
+                    return;
+                }
             }
 
-              System.IO.File.WriteAllBytes(TotalPath, screenshotTexture1.EncodeToPNG());
+            #endregion
+            //Ver 1 . NPC만                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     c                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+            #region
+            //for (int i = 0; i < SuperManager.Instance.buildingManager.buildings.Count; i++)
+            //{
+            //    Debug.Log(SuperManager.Instance.buildingManager.buildings[i]._livingCharacter);
+            //    if (PageMat1[i].GetTexture("_MainTex") == null)// 사진도 비어있고
+            //    {
+            //        if (SuperManager.Instance.buildingManager.buildings[i] != null) // 빌딩스에 값이 할당됐다면
+            //        {
+            //            if (SuperManager.Instance.buildingManager.buildings[i]._livingCharacter != null)
+            //            {
+            //                Debug.Log("테리우스 김채원 닮음");
+            //                PageMat1[i].SetTexture("_MainTex", Texutre2);
+            //                PageMat1[i + 1].SetTexture("_MainTex", Texutre1);
+            //                camera.targetTexture = null;
+            //                Destroy(RenderTexture);
+            //                return;
+            //            }
+            //            else
+            //            {
+            //                camera.targetTexture = null;
+            //                Destroy(RenderTexture);
+            //                continue;
+            //            }
+            //        }
+            //    }
+            //}
+            #endregion
+
+
+            System.IO.File.WriteAllBytes(TotalPath, screenshotTexture1.EncodeToPNG());
             System.IO.File.WriteAllBytes(TotalPath, screenshotTexture2.EncodeToPNG());
             camera.targetTexture = null;
             Destroy(RenderTexture);
