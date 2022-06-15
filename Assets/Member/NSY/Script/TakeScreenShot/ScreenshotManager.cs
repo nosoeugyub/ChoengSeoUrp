8caf3ab3f5b9d61314849395ef34051c596bd834
nosoeugyub <74639167+nosoeugyub@users.noreply.github.com>
2022-06-16 07:45:40 +0900

    스크린샷완로

--- Assets/Member/NSY/Script/TakeScreenShot/ScreenshotManager.cs
+++ Assets/Member/NSY/Script/TakeScreenShot/ScreenshotManager.cs
@@ -6,27 +6,5 @@ public class ScreenshotManager : MonoBehaviour
 {
     public List<Screenshot> screenshots;
 
-    public void screenshosEvent()
-    {
-        for (int i = 0; i < screenshots.Count; i++)
-        {
-            Debug.Log("씨발아 김취라");
-            screenshots[i].OnSceenShotEvent();
-        }
-    }
 
-    //private void onable()
-    //{
-    //    for (int i = 0; i < screenshot.Count; i++)
-    //    {
-    //        screenshot[i].OnSceenShot += screenshot[i].OnSceenShotEvent;
-    //    }
-    //}
-    //private void OnDisable()
-    //{
-    //    for (int i = 0; i < screenshot.Count; i++)
-    //    {
-    //        screenshot[i].OnSceenShot -= screenshot[i].OnSceenShotEvent;
-    //    }
-    //}
 }
