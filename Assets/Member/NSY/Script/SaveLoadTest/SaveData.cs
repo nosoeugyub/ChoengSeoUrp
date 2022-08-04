292e83b06edffc60579323b3ac5dbd79e3641017
nosoeugyub <74639167+nosoeugyub@users.noreply.github.com>
2022-08-04 14:31:41 +0900

    야붕이 커밋

---
+++ Assets/Member/NSY/Script/SaveLoadTest/SaveData.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveData : MonoBehaviour
+{
+   private void Save()
+    {
+        SaveDataInfo SaveDatainfo = new SaveDataInfo
+        {
+        //    PlayerPostion = 
+        };
+        string _saveJson = JsonUtility.ToJson(SaveDatainfo);
+
+        File.WriteAllText(Application.dataPath + "/save.txt", _saveJson);
+
+    }
+    private void Load()
+    {
+
+    }
+}
+
+public class SaveDataInfo
+{
+    public Vector3 PlayerPostion;
+
+   
+   public class ItemslotSaveData
+    {
+        public string ItemID;
+        public int Amount;
+
+        public ItemslotSaveData(string id, int amount)
+        {
+            ItemID = id;
+            Amount = amount;
+        }
+
+    }
+    public class itemContainerSaveData
+    {
+        public ItemslotSaveData[] SaveSlot;
+
+        public itemContainerSaveData(int numItems)
+        {
+            SaveSlot = new ItemslotSaveData[numItems];
+        }
+    }
+
+}
---
+++ Assets/Member/NSY/Script/SaveLoadTest/SaveManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class SaveManager : MonoBehaviour
+{
+    public static readonly string SAVE_FOLDER = Application.dataPath + "/Saves/";
+
+    public static void Init()
+    {
+        if (!Directory.Exists(SAVE_FOLDER))
+        {
+            Directory.CreateDirectory(SAVE_FOLDER);
+        }
+    }
+
+    public static void Save(string saveString)
+    {
+        File.WriteAllText(SAVE_FOLDER + "save.txt", saveString);
+    }
+    public static string Load()
+    {
+        if (File.Exists(SAVE_FOLDER + "/save.txt"))
+        {
+            string saveString = File.ReadAllText(SAVE_FOLDER + "/save.txt");
+            return saveString;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
