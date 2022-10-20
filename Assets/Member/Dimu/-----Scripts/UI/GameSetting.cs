ff4918fa07ec047c46d35f7d30bcae81c932215f
Kim Chaewon <48170768+kimdimu@users.noreply.github.com>
2022-10-21 03:15:26 +0900

    설정과 건축 이벤트 충돌 해결

--- Assets/Member/Dimu/-----Scripts/UI/GameSetting.cs
+++ Assets/Member/Dimu/-----Scripts/UI/GameSetting.cs
@@ -36,16 +36,21 @@ public class GameSetting : MonoBehaviour
         }
         settingUI.SetActive(!settingUI.activeSelf);
     }
+    public void ClickCloseSetting()
+    {
+        PlayerInput.OnPressESCDown = savedelegate;
+        settingUI.SetActive(false);
+    }
     public void ReflectionONOff(bool value)
     {
         StartCoroutine(ReflectionWithFader());
     }
     IEnumerator ReflectionWithFader()
     {
-        yield return fader.IFadeOut(Color.white,0.3f);
+        yield return fader.IFadeOut(Color.white, 0.3f);
         reflection.enabled = !reflection.isActiveAndEnabled;
         yield return new WaitForSeconds(0.5f);
-        yield return fader.IFadeIn(Color.white,0.3f);
+        yield return fader.IFadeIn(Color.white, 0.3f);
     }
     public void ExitGame()
     {
